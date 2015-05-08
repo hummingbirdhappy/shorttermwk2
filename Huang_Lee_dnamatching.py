d1a2e20d29322ac615f927e3d2a60b14a3744042
unknown <yhuang@A22463.bcis.bates.edu>
2015-05-08 16:15:27 -0400

    assignment

--- Huang_Lee_dnamatching.py
+++ Huang_Lee_dnamatching.py
@@ -1,33 +1,34 @@
 string1 = raw_input("Please enter a string using the letters A, T, C, and G: ")
+#for l in string1:
+    #if l not in "ATCG":
+        #string1 = raw_input("Please enter a string using only the letters A, T, C, and G: ")
+    #else:
+        #pass
 string2 = raw_input("Please enter another string using the letters A, T, C, and G: ")
 
 #prompt for one of four commandss:
-print "What do you want to do: "
-print "a (add indel)"
-print "d (delete indel)"
-print "s (score)"
-print "q (quit):"
+indel_change = raw_input("What do you want to do: \n a (add indel) \n d (delete indel) \n s(score) \n q (quit): ")
 # need to print list of possible commands together and specify raw_input as the response to this printed statement
-if raw_input == "a":
+if indel_change == "a":
     change_a = raw_input("Which string do you want to change?")
     index_a = raw_input("Before which index do you wish to place the indel?")
     if index_a.isdigit():
         index_a = int(index_a)
     else:
-        print ("Please enter an integer for your index.")
-    for i in range(len(change_a)):
-        if index_a > i:
-            print "This index is out of range."
-        else:
-            print change_a[0,i-1]+'-'+change_a[i,len(change_a)]
+        index_a = raw_input("Please enter an integer for the index: ")
+
+    if index_a > ((len(change_a))-1):
+        print "This index is out of range."
+    else:
+        print change_a[0,((index_a)-1)]+'-'+change_a[index_a,len(change_a)]
         
-elif raw_input == "d":
-    string_change_d = raw_input("Which string do you want to change?")
+elif indel_change == "d":
+    change_d = raw_input("Which string do you want to change?")
     index_d = raw_input("At which index do you want to delete the indel?")
     if index_d.isdigit():
         index_d = int(index_d)
     else:
-        print ("Please enter an integer for your index.")
+        index_d = raw_input("Please enter an integer for your index.")
 
     
 
